f46a40d1c0c4ea9f73850985344f135072340279
Amit Bamane <amitbamane26@gmail.com>
2021-11-01 19:03:42 +0530

    Solved postgres connection error.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask
 from flask_restful import Api
@@ -8,8 +9,13 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList   
 from resources.store import Store, StoreList
 
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    
 app.secret_key = 'key'
 api = Api(app)
